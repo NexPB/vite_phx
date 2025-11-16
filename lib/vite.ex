79fbf984e2769faa32427b73ee894b250bf0badf
NexPB <nex.powerbot@hotmail.com>
2025-11-16 17:48:04 +0900

    hotfix: call `entries/0`

--- lib/vite.ex
+++ lib/vite.ex
@@ -10,13 +10,14 @@ defmodule Vite do
   attr :entries, :list
 
   def head(assigns) do
-    assigns = assign_new(assigns, :entries, fn ->
-      if production?() do
-        Vite.Manifest.entries(assigns[:src])
-      else
-        []
-      end
-    end)
+    assigns =
+      assign_new(assigns, :entries, fn ->
+        if production?() do
+          Vite.Manifest.entries()
+        else
+          []
+        end
+      end)
 
     if not production?() do
       ~H"""
